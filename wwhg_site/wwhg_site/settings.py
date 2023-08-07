9ecef8d1e4abeb36b7efcc3d8bd871542024e41c
Siim Kängsepp <siim.kangsepp@gmail.com>
2023-08-07 23:40:41 +0300

    Added MySQL connection

--- wwhg_site/wwhg_site/settings.py
+++ wwhg_site/wwhg_site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wwhg_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wwhg_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'abc123',
+        'PORT': '3306',
     }
 }
 
