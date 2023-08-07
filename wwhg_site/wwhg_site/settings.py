31005ad83dec27397b331e9b128c230922f36637
Le-Sova <122235643+Le-Sova@users.noreply.github.com>
2023-08-08 00:46:46 +0300

    Test

--- wwhg_site/wwhg_site/settings.py
+++ wwhg_site/wwhg_site/settings.py
@@ -80,12 +80,12 @@ DATABASES = {
         'NAME': 'wwhg_db',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': 'Vargtass02',
+        'PASSWORD': 'abc123',
         'PORT': '3306',
     }
 }
 
-
+#
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
