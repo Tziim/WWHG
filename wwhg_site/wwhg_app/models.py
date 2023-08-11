d91559e270da50e623c66629dc48ec08b5369712
arela <arelaurand@hotmail.com>
2023-08-12 01:22:02 +0300

    Tegin juurde kaks HTML, lisasin urls path`id, muutsin Item Product nimeks. Hetkel kõik avanes ja töötas. Suur töö on nüüd ees kõikide lisalehtede tegemine ja ägeda kujunduse loomine. Hetkel ikka nagu aastast 90 :)

--- wwhg_site/wwhg_app/models.py
+++ wwhg_site/wwhg_app/models.py
@@ -12,8 +12,16 @@ def filepath(request, filename):
     return os.path.join('uploads/', filename)
 
 
-class Item(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
     name = models.TextField(max_length=191)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.TextField(max_length=50)
     description = models.TextField(max_length=500, null=True)
     image = models.ImageField(upload_to=filepath, null=True, blank=True)
@@ -25,3 +33,12 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
     address = models.TextField()
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
--- wwhg_site/wwhg_app/views.py
+++ wwhg_site/wwhg_app/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "wwhg_app/index.html", {})
+
+
+def all_products(request):
+    # Fetch all products from the database and pass them to the template
+    # Replace this with actual database querying logic
+    products = []
+    return render(request, 'wwhg_app/shop/all_products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    # Fetch the specific product from the database based on product_id
+    # Replace this with actual database querying logic
+    product = None
+    return render(request, 'wwhg_app/shop/product_detail.html', {'product': product})
