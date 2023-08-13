bf798b93f28070f6523047ead8ff33bd70dc2658
Siim Kängsepp <siim.kangsepp@gmail.com>
2023-08-13 03:58:41 +0300

    Added products to database

--- wwhg_site/wwhg_app/models.py
+++ wwhg_site/wwhg_app/models.py
@@ -7,21 +7,33 @@ import os
 
 def filepath(request, filename):
     old_filename = filename
-    timeNow = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    filename = "%s%s", (timeNow, old_filename)
-    return os.path.join('uploads/', filename)
+    time_now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    new_filename = "{}{}".format(time_now, old_filename)
+    return os.path.join('uploads/', new_filename)
 
 
-class Item(models.Model):
-    name = models.TextField(max_length=191)
-    price = models.TextField(max_length=50)
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return f" Category {self.name}"
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=191, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.TextField(max_length=50, null=False)
     description = models.TextField(max_length=500, null=True)
     image = models.ImageField(upload_to=filepath, null=True, blank=True)
 
+    def __str__(self):
+        return f" Product {self.name}"
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    address = models.TextField()
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
