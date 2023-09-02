ff01f470c0875fa210b0e4fdda28c292b3b9c218
Siim Kängsepp <siim.kangsepp@gmail.com>
2023-09-03 00:19:31 +0300

    Order history poolik areng

--- wwhg_site/static/js/orders.js
+++ wwhg_site/static/js/orders.js
@@ -1,27 +1,24 @@
 // orders.js
-document.addEventListener('DOMContentLoaded', function () {
-  const orderButtons = document.querySelectorAll('.order-details-button');
-  const popup = document.getElementById('order-popup');
-  const popupContent = popup.querySelector('.popup-content');
-  const closePopup = popup.querySelector('.close-popup');
 
-  orderButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const orderId = button.getAttribute('data-order-id');
-      // Make an AJAX request to retrieve order details and populate popupContent.
-      // Once you have the order details, populate popupContent.
-      // Finally, show the popup.
-      popup.style.display = 'block';
-    });
-  });
+// Function to open the popup window
+function openPopup(orderId) {
+    // You can customize the popup appearance and content here
+    const popupContent = `
+        <div class="popup-content">
+            <h2>Order Details</h2>
+            <p>Order ID: ${orderId}</p>
+            <!-- Add more order details here -->
+        </div>
+    `;
 
-  closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
-});
+    // Create a new window with the popup content
+    const popupWindow = window.open('', 'OrderPopup', 'width=400,height=300');
+    popupWindow.document.write(popupContent);
+}
 
-
-document.getElementById("confirmPaymentButton").addEventListener("click", function () {
-    // Redirect to the 'create_order' URL when the button is clicked
-    window.location.href = "{% url 'create_order' %}";
+// Add click event listeners to each order row
+const orderRows = document.querySelectorAll('.order-row');
+orderRows.forEach(row => {
+    const orderId = row.getAttribute('data-order-id');
+    row.addEventListener('click', () => openPopup(orderId));
 });
