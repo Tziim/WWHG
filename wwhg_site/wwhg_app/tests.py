bc6fcc89a7ea3741f5107368a2f0a2dfcf9e816d
Siim Kängsepp <siim.kangsepp@gmail.com>
2023-09-02 18:11:55 +0300

    Testide põhi + kokku keevitatud

--- wwhg_site/wwhg_app/tests.py
+++ wwhg_site/wwhg_app/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
-from django.test import TestCase, Client
+
+from .models import Product, Category
 
 
+class ModelsTestCase(TestCase):
 
-def test_category_creation():
-    category = Category.objects.create(name="Test Category")
-    saved_category = Category.objects.get(name="Test Category")
-    assert category == saved_category
+    def setUp(self):
+        self.category = Category.objects.create(name="Test")
+        self.product = Product.objects.create(
+            name="Test",
+            category=self.category,
+            price=69.9,
+            description="Test"
 
+        )
+
+    def test_creation(self):
+        pass
